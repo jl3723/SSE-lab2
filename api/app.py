3d8743ecf3d87282efba83d421d767d51881bd7a
jl3723 <jl3723@imperial.ac.uk>
2023-10-16 16:40:58 +0100

    folder

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+    
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
