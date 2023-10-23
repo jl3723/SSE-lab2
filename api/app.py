903be6d1568c7594e3520b77a4c46dfa85f53ac3
jl3723 <jl3723@imperial.ac.uk>
2023-10-23 17:18:35 +0100

    Query

--- api/app.py
+++ api/app.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)   
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
-    
+
+def process_query(name):
+    if name == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+
+    if name == "asteroids":
+        return "Unknown"
+
+    if name == "q":
+        return "dinosaurs"
+
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
-    return render_template("hello.html", name=input_name, age=input_age)   
+    return render_template("hello.html", name=input_name, age=input_age)
 
-
-
+@app.route("/query", methods=["GET"])
+def handle_query():
+    q_query = request.args.get('q')
+    process_query(q_query)
